bb848e4153c6b08af03814a1df2c2c1d32e174eb
prisbre <huanfeng.c@gmail.com>
2016-06-15 22:22:00 +0800

    feat: task2_5 adding insert interaction

--- js/task0002_5.js
+++ js/task0002_5.js
@@ -7,7 +7,7 @@
         return event.target || event.srcElement;
     };
 
-    // draggable element behavior & transfer data
+    // draggable behavior & transfer data
     function dgStart(event) {
         event = $.getEvent(event);
         target = $.getTarget(event);
@@ -21,10 +21,12 @@
     // drag over behavior
     function dgOver(event) {
         event = $.getEvent(event);
+        target = $.getTarget(event);
         event.preventDefault();
         event.dataTransfer.dropEffect = 'move';
     };
     delegateEvent($('#main'), 'ul', 'dragover', dgOver);
+    delegateEvent($('#main'), 'li', 'dragover', dgOver);
 
     // drop spot behavior
     function dropHandler(event) {
@@ -32,11 +34,48 @@
         target = $.getTarget(event);
         event.preventDefault();
         var data = event.dataTransfer.getData('text');
-        console.log(data);
-        target.appendChild(document.getElementById(data));
-        event.dataTransfer.clearDate();
+        var tag = target.tagName.toLowerCase();
+
+        // check container status, return true when not full
+        function notFull(target, tag) {
+
+            var list;
+            switch (tag) {
+                case 'ul':
+                    list = target.getElementsByTagName('li');console.log(3);
+                    console.log(target, list, list.length);
+                    break;
+                case 'li':
+                    list = target.parentNode.getElementsByTagName('li');
+                    break;
+            };
+            console.log(list);
+            if (list.length < 6) {
+                return true;
+            } else {
+                return false;
+            };
+        };
+
+        if (notFull(target, tag)) {
+            console.log(2);
+
+            // append element when drop to the end
+            if (tag == 'ul') {
+                target.appendChild(document.getElementById(data));
+                console.log(1);
+            // insert element when drop on other element
+            } else if (tag == 'li') {
+                event.stopPropagation();
+                target.parentNode.insertBefore(document.getElementById(data), target);
+            };
+            event.dataTransfer.clearDate();
+        } else {
+            return;
+        };
     };
     delegateEvent($('#main'), 'ul', 'drop', dropHandler);
+    delegateEvent($('#main'), 'li', 'drop', dropHandler);
 
 
 }) ();
