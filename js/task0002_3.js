0011ddfd1f78ccf0301de68ee515b08f4c76c0a5
prisbre <huanfeng.c@gmail.com>
2016-06-18 11:34:04 +0800

    feat: task2_3.js wrap up arrowClick()

--- js/task0002_3.js
+++ js/task0002_3.js
@@ -11,28 +11,47 @@
     function animate(event) {
         event = $.getEvent(event);
         target = $.getTarget(event);
-
-        function slide(imgWidth, num, direction, loop, interval) {};
-
-        // arrow click animation
         var imgWidth = 600,
             num = 4,
             id = target.getAttribute('id'),
             container = $('#container'),
-            leftVal = parseInt(window.getComputedStyle(container).left);
-        if (id == 'prev') {
-            if (leftVal > -imgWidth) {
-                leftVal = -imgWidth * num;
+            leftVal = parseInt(window.getComputedStyle(container).left);    // control slide position
+            index = 0;    // mark button index
+
+        function slide(imgWidth, num, direction, loop, interval) {};
+
+        // arrow click animation
+        function arrowClick(id) {
+            if (id == 'prev') {
+                if (leftVal > -imgWidth) {
+                    leftVal = -imgWidth * num;
+                };
+                leftVal += imgWidth;
+            } else if (id == 'next') {
+                if (leftVal < -imgWidth * num) {
+                    leftVal = -imgWidth;
+                };
+                leftVal -= imgWidth;
             };
-            leftVal += imgWidth;
-        } else if (id == 'next') {
-            if (leftVal < -imgWidth * num) {
-                leftVal = -imgWidth;
-            };
-            leftVal -= imgWidth;
+            container.style.setProperty('left', leftVal + 'px');
         };
-        container.style.setProperty('left', leftVal + 'px');
-    };
+
+
+        if (target.tagName.toLowerCase() == 'a') {
+            arrowClick(id);
+        };
+
+        // button click animation
+        function buttonClick(event) {
+            index
+        }
+
+
+
+
+    }
+
+
 
 
     delegateEvent($('article'), 'a', 'click', animate);
