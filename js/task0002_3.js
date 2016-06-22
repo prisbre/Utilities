3278eee111df9b45effe16f1ae95b2c4bf85537e
prisbre <huanfeng.c@gmail.com>
2016-06-22 10:55:33 +0800

    feat: task2_3.js decoupling buttonClick() activeDot() slide()

--- js/task0002_3.js
+++ js/task0002_3.js
@@ -7,7 +7,102 @@
         return event.target || event.srcElement;
     };
 
-    // carousel animation
+
+
+    // get left value of current picture
+    function getLeft(){
+        var container = $('#container'),
+        leftNow = parseInt(window.getComputedStyle(container).left);
+        return leftNow;
+    };
+
+
+    // slide to target picture
+    function slide(leftVal, imgWidth, num) {
+        console.log(leftVal);
+
+        // slide transition
+        container.style.setProperty('transition-duration', '0.3s');
+        container.style.setProperty('transform', 'translate(' + leftVal + 'px' + ', 0px)');
+/*
+        // circulate
+        var circulate = true;
+        if (circulate) {
+            if (leftVal > -imgWidth) {
+                leftVal = -imgWidth * (num - 1);
+            };
+            if (leftVal < -imgWidth * (num + 1)) {
+                leftVal = 0;
+            };
+            container.style.setProperty('transform', 'translate(' + leftVal + 'px' + ', 0px)');
+            // container.style.setProperty('left', leftVal + 'px');
+        };*/
+
+        return;
+    };
+
+    // button style: active dot toggle
+    function activeDot(index) {
+        var activeElement = $('#on');
+        if (activeElement) {
+            $('#on').removeAttribute('id');
+        };
+        // console.log(index, document.getElementsByClassName('btn'));
+        document.getElementsByClassName('btn')[index - 1].setAttribute('id', 'on');
+    };
+
+    // button click animation
+    function buttonClick(target, imgWidth, num) {
+        // switch image
+        var leftVal = getLeft();
+        index = parseInt(target.dataset.index);
+        if (leftVal != -imgWidth * (index)) {
+            leftVal = -imgWidth * (index);
+        };
+        console.log(index)
+        slide(leftVal, imgWidth, num);
+
+        // toggle button style
+        activeDot(index);
+    };
+
+
+    delegateEvent(
+        $('article'),
+        'li',
+        'click',
+        function (event) {
+            var event = $.getEvent(event),
+                target = $.getTarget(event),
+                imgWidth = 600,
+                num = 4;
+
+            return buttonClick(target, imgWidth, num);
+
+        });
+
+
+
+    //delegateEvent($('article'), 'a', 'click', animate);
+    // delegateEvent($('article'), 'li', 'click', animate);
+    //delegateEvent($('form'), 'span', 'click', controlInfo);
+}) ();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+    // animation handler
     function animate(event) {
         event = $.getEvent(event);
         target = $.getTarget(event);
@@ -36,7 +131,7 @@
                 leftVal -= imgWidth;
                 index ++;
             };
-
+            slide(leftVal);
             // change button style
             return activeDot(index);
         };
@@ -70,14 +165,18 @@
             buttonClick(target);
         };
 
-        container.style.setProperty('left', leftVal + 'px');
+        // container.style.setProperty('left', leftVal + 'px');
+        slide(leftVal, direction, loop, duration);
     };
 
-    // autoplay animation
-    function slide(imgWidth, num, direction, loop, interval) {
-    };
 
-    // carousel setting value
+    carousel.timer = setTimeout(function refer() {
+        slide(leftVal, direction, loop, duration).call(controlInfo);
+        carousel.timer = setTimeout(refer, duration);
+    }, duration);
+
+
+    // setting handler
     function controlInfo(event) {
         event = $.getEvent(event);
         target = $.getTarget(event);
@@ -85,22 +184,16 @@
             loop =  $('form').loop.value,
             interval = $('input').value;
 
-        return slide(imgWidth, num, direction, loop, interval);
+        // set timer & autoplay
+        if (controlInfo.timer) {
+            clearTimeout(controlInfo.timer);
+        };
+        controlInfo.timer = setTimeout(function refer() {
+            slide(leftVal, direction, loop, duration).call(controlInfo);
+            controlInfo.timer = setTimeout(refer, interval);
+        }, interval);
 
     };
-/*
-    // slide setting
-    //,
+*/
 
 
-
-
-
-    delegateEvent($('article'), 'a', 'click', animate);
-    delegateEvent($('article'), 'li', 'click', animate);
-    delegateEvent($('form'), 'span', 'click', controlInfo);
-}) ();
-
-
-
-// carousel.timer = setTimeout(interval)
